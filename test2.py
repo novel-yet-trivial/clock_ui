fe563462756572fd70322be03ca93bd5ced3e9d1
nyt <jonsspamaccout@gmail.com>
2016-12-07 14:09:10 -0800

    some cleanup

--- test2.py
+++ test2.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 __author__ = 'Geoff'
 from Tkinter import StringVar
 import Tkinter as tk   # python
@@ -9,62 +11,54 @@ import time
 TITLE_FONT = ("Helvetica", 18, "bold")
 
 
-class SampleApp(tk.Tk):
+class SampleApp(tk.Frame):
+    """The container frame"""
 
     def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-
-        # the container is where we'll stack a bunch of frames
-        # on top of each other, then the one we want visible
-        # will be raised above the others
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-        self.maxsize(480,320);
-        self.minsize(480,320);
-
+        tk.Frame.__init__(self, *args, **kwargs)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
         for F in (StartPage, PageOne, PageTwo):
-            page_name = F.__name__
-            frame = F(parent=container, controller=self)
+            frame = F(self)
             # frame.configure(background = 'black')
-            self.frames[page_name] = frame
+            self.frames[F] = frame #we may as well use the class itself as the key, instead of the name
 
             # put all of the pages in the same location;
             # the one on the top of the stacking order
             # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
 
-
-        self.show_frame("StartPage")
-        self.update_idletasks()
-
+        self.show_frame(StartPage)
+        #update_idletasks() should be needed very rarely
 
     def show_frame(self, page_name):
         '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
-        self.update_idletasks()
 
-
+# button attributes. It's good to put things like this in a single place
+# so that it's easy to change all the buttons at once
+opt = {"foreground": "black", "background":"grey"}
 
 class StartPage(tk.Frame):
 
-    def __init__(self, parent, controller):
+    def __init__(self, parent):
         tk.Frame.__init__(self, parent)
-        self.controller = controller
+        #tkinter automatically assigns parent to "self.master"
+
         self.time_var = StringVar()
 
         label = tk.Label(self, textvariable=self.time_var, font=TITLE_FONT)
         label.pack(side="top", fill="x", pady=10)
 
-        button1 = tk.Button(self, text="Go to Page One",
-                            command=lambda: controller.show_frame("PageOne"), foreground="black", background="grey")
-        button2 = tk.Button(self, text="Go to Page Two",
-                            command=lambda: controller.show_frame("PageTwo"), foreground="black", background="grey")
+        button1 = tk.Button(self, text="Go to Page One", **opt)
+        button1.config(command=lambda: parent.show_frame(PageOne)) #same thing as yours, just neater (in my opinion)
         button1.pack()
+
+        button2 = tk.Button(self, text="Go to Page Two", **opt)
+        button2.config(command=lambda: parent.show_frame(PageTwo))
         button2.pack()
 
         self.update_time()
@@ -75,46 +69,40 @@ class StartPage(tk.Frame):
 
 class PageOne(tk.Frame):
 
-    def __init__(self, parent, controller):
+    def __init__(self, parent):
         tk.Frame.__init__(self, parent)
-        self.controller = controller
         label = tk.Label(self, text="Alarm", font=TITLE_FONT)
         label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
+        button = tk.Button(self, text="Go to the start page")
+        button.config(command=lambda: parent.show_frame(StartPage))
         button.pack()
 
 
 class PageTwo(tk.Frame):
 
-    def __init__(self, parent, controller):
+    def __init__(self, parent):
         tk.Frame.__init__(self, parent)
-        self.controller = controller
         label = tk.Label(self, text="Choose Music", font=TITLE_FONT)
         label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
+        button = tk.Button(self, text="Go to the start page")
+        button.config(command=lambda: parent.show_frame(StartPage))
         button.pack()
-        controller.after(200, controller.show_frame("StartPage"))
+
+        # what was the point of this? It's useless.
+        # controller.after(200, controller.show_frame("StartPage"))
+
 
 def print_log():
     print "1 second"
 
-# class PageTwo(tk.Frame):
-#
-#     def __init__(self, parent, controller):
-#         self.after(1000, tk.Frame.__init__(self, parent))
-#         self.controller = controller
-#         time_var = StringVar()
-#         time_var.set(time.strftime("%H:%M:%S"))
-#         label = tk.Label(parent, textvariable=time_var, font=TITLE_FONT)
-#         label.pack(side="top", fill="x", pady=10)
-#         button = tk.Button(self, text="Go to the start page",
-#                            command=lambda: controller.show_frame("StartPage"))
-#         button.pack()
-
-
+def main():
+    # this could be a class but in general we try not to use classes when a function will do
+    root = tk.Tk()
+    app = SampleApp(root)
+    app.pack(side="top", fill="both", expand=True)
+    root.geometry("480x320")
+    root.resizable(0, 0)
+    root.mainloop()
 
 if __name__ == "__main__":
-    app = SampleApp()
-    app.mainloop()
+    main()
